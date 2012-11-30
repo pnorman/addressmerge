fa26df98436c61c87a2e5f59bc65302e5644d48b
Paul Norman <penorman@mac.com>
2012-11-30 08:42:14 -0800

    initial pass at a mapping file

---
+++ addressmapping.py
@@ -0,0 +1,84 @@
+# This file is based on geocodemapping.py of imposm.geocoder.
+#
+# geocodemapping.py Copyright 2012 Omniscale (http://omniscale.com)
+#
+# addressmapping.py Copyright 2012 Paul Norman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from imposm.mapping import (
+    Options,
+    Points, LineStrings, Polygons,
+    String, Bool, Integer, OneOfInt,
+    set_default_name_type, LocalizedName,
+    WayZOrder, ZOrder, Direction,
+    GeneralizedTable, UnionView,
+    PseudoArea, meter_to_mapunit, sqr_meter_to_mapunit,
+)
+
+db_conf = Options(
+    # db='osm',
+    host='localhost',
+    port=5432,
+    user='osm',
+    password='osm',
+    sslmode='allow',
+    prefix='osm_new_',
+    proj='epsg:4326',
+)
+
+point_addresses = Points(
+    name = 'point_addresses',
+    with_type_field = False,
+    fields = (
+        ('addr:street', String()),
+        ('addr:postcode', String()),
+        ('addr:city', String()),
+        ('addr:country', String()),
+        ('addr:housenumber', String()),
+    ),
+    mapping = {
+        'addr:housenumber': (
+            '__any__',
+        ),
+    }
+)
+
+polygon_addresses = Polygons(
+    name = 'polygon_addresses',
+    with_type_field = False,
+    fields = (
+        ('addr:street', String()),
+        ('addr:postcode', String()),
+        ('addr:city', String()),
+        ('addr:country', String()),
+        ('addr:housenumber', String()),
+    ),
+    mapping = {
+        'addr:housenumber': (
+            '__any__',
+        ),
+    }
+)
+addresses = UnionView(
+    name = 'addresses',
+    fields = (
+        ('addr:street', String()),
+        ('addr:postcode', String()),
+        ('addr:city', String()),
+        ('addr:country', String()),
+        ('addr:housenumber', String()),
+    ),
+    mappings = [point_addresses, polygon_addresses],
+)
