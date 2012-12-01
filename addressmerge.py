1e4c10aa8c8167ab0bd64e412d648e645c2e4ebc
Paul Norman <penorman@mac.com>
2012-12-01 12:00:07 -0800

    Add database arguments

--- addressmerge.py
+++ addressmerge.py
@@ -5,3 +5,14 @@ import psycopg2
 import argparse
 import logging as l
 l.basicConfig(level=l.DEBUG, format="%(message)s")
+
+parser = argparse.ArgumentParser(description='Conflate an address file with existing OSM data')
+
+# Database options
+parser.add_argument('-d', '--dbname', default='osm', help='Database to connect to. Defaults to osm.')
+parser.add_argument('-U', '--username', default='osm', help='Username for database. Defaults to osm.')
+parser.add_argument('--host', default='localhost', help='Hostname for database. Defaults to localhost.')
+parser.add_argument('-p', '--port', default=5432, type=int, help='Port for database. Defaults to 5432.')
+parser.add_argument('-P', '--password', default='osm',  help='Password for database. Defaults to osm.')
+
+args = parser.parse_args()
