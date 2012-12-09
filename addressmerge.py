22601eb478ee8f7f6b0e49c70afe9b8716dd4ef3
Paul Norman <penorman@mac.com>
2012-12-08 23:46:15 -0800

    Release of 0.1
    This version will remove address nodes already present in OSM

--- addressmerge.py
+++ addressmerge.py
@@ -12,6 +12,7 @@ import psycopg2
 
 # .osm modules
 from imposm.parser import OSMParser
+from lxml import etree
 
 class OSMSource(object):
     def __init__(self, database, user, password, host, port):
@@ -19,13 +20,65 @@ class OSMSource(object):
         self._conn=psycopg2.connect(database=database, user=user, 
                                     password=password, host=host, 
                                     port=str(port))
-        self._conn.set_session(readonly=True, autocommit=True)
+        self._conn.set_session(readonly=False, autocommit=False)
+        self._table_created = False
 
+    def create_table(self):
+        curs = None
+        try:
+            curs = self._conn.cursor()
+            curs.execute('''CREATE TEMPORARY TABLE import_addresses (
+                            import_id integer,
+                            "addr:housenumber" varchar(255),
+                            "addr:street" varchar(255),
+                            "addr:city" varchar(255));''')
+            self._table_created = True
+            curs.connection.commit()
+        except BaseException:
+            if curs is not None:
+                curs.connection.rollback()
+            raise
+        finally:
+            if curs is not None:
+                curs.close()
+
+    def load_addresses(self, addresses):
+        self.create_table()
+        curs = None
+        try:
+            curs = self._conn.cursor()
+            for (id, tags, _) in addresses:
+                curs.execute('''INSERT INTO import_addresses
+                                (import_id, "addr:housenumber", "addr:street", "addr:city")
+                                VALUES (%s, %s, %s, %s);''',
+                                (id, tags['addr:housenumber'], tags['addr:street'], tags['addr:city']))
+            curs.execute('''ANALYZE;''')
+            curs.connection.commit()
+        except BaseException:
+            if curs is not None:
+                curs.connection.rollback()
+            raise
+        finally:
+            if curs is not None:
+                curs.close()
+
+    def find_duplicates(self):
+        curs = None
+        try:
+            curs = self._conn.cursor()
+            curs.execute('''SELECT DISTINCT ON (import_id) import_id FROM osm_addresses JOIN import_addresses USING ("addr:housenumber", "addr:street", "addr:city");''')
+            return set(id[0] for id in curs.fetchall())
+        except BaseException:
+            if curs is not None:
+                curs.connection.rollback()
+            raise
+        finally:
+            if curs is not None:
+                curs.close()
 
 class ImportDocument(object):
     newNodes = []
-    def __init__(self, chunk=1):
-        self.chunk = chunk
+    def __init__(self):
         self._parser = OSMParser(nodes_callback=self._parse_nodes)
         self._nodes = deque()
 
@@ -37,24 +90,31 @@ class ImportDocument(object):
         for node in nodes:
             self._nodes.append(node)
 
+    def _serialize_node(self, f, node):
+        xmlnode = etree.Element('node', {'visible':'true', 'id':str(node[0]), 'lon':str(node[2][0]), 'lat':str(node[2][1])})
+        for (k,v) in node[1].items():
+            tag = etree.Element('tag',  {'k':k, 'v':v})
+            xmlnode.append(tag)
+
+        f.write(etree.tostring(xmlnode))
+        f.write('\n')
+
     def remove_existing(self, source):
         ''' This function will remove addresses that already exist in OSM'''
-        # Make a local copy that we'll delete from as we process
-        self._localnodes = copy.copy(self._nodes)
-        while len(self._localnodes) > 0:
-            self._process_chunk(source)
+        l.debug('Loading addresses')
+        source.load_addresses(self._nodes)
+        l.debug('Finding duplicates')
+        duplicates = source.find_duplicates()
+        l.debug('Removing duplicates')
+        self._nodes = filter(lambda node: node[0] not in duplicates, self._nodes)
+        l.debug('%d duplicates removed', len(duplicates))
 
-    def _process_chunk(self, source):
-        ''' Pops a deque of up to self.chunk nodes off and parses them '''
-        request_list = deque()
-        cur = self.
-        try:
-            for _i in xrange(0,self.chunk):
-                request_list.append(self._localnodes.pop())
-        except IndexError:
-            pass
-        # len(request_list) is now 0 to N
-        
+    def output_osm(self, f):
+        f.write('<?xml version="1.0"?>\n<osm version="0.6" upload="false" generator="addressmerge">\n')
+        for node in self._nodes:
+            self._serialize_node(f, node)
+        f.write('</osm>')
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Conflate an address file with existing OSM data')
@@ -70,12 +130,11 @@ if __name__ == '__main__':
     parser.add_argument('--host', default='localhost', help='Hostname for database. Defaults to localhost.')
     parser.add_argument('-p', '--port', default=5432, type=int, help='Port for database. Defaults to 5432.')
     parser.add_argument('-P', '--password', default='osm',  help='Password for database. Defaults to osm.')
-    parser.add_argument('--chunk', default=13, type=int, help='Number of records requested at once')
 
     # .osm parser options
-    parser.add_argument('--threads', default=1, type=int, help='Threads to use when parsing the input OSM file')
+    parser.add_argument('--threads', default=None, type=int, help='Threads to use when parsing the input OSM file')
     parser.add_argument('input', help='Input OSM file')
-    parser.add_argument('output', help='Output OSM file')
+    parser.add_argument('output', type=argparse.FileType('w'), help='Output OSM file')
 
     args = parser.parse_args()
 
@@ -83,7 +142,8 @@ if __name__ == '__main__':
                           password=args.password, host=args.host,
                           port=str(args.port))
 
-    source = ImportDocument(chunk=args.chunk)
+    source = ImportDocument()
 
     source.parse_osm(args.input)
     source.remove_existing(existing)
+    source.output_osm(args.output)
