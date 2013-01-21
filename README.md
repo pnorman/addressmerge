c4a26eb6d66f2b21d984f73720fc3c4779edbb9b
Paul Norman <penorman@mac.com>
2013-01-20 19:42:36 -0800

    Switch to a pgsnapshot database instead of an imposm one
    This closes #6

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ A tool for merging address data with OSM data
 
 ## Installation ##
 
-addressmerge requires psycopg2 and imposm.parser. It also requires access to a postgresql database with OSM data for the area imported with imposm. This database will normally be on the same machine as addressmerge although it does not have to be.
+addressmerge requires psycopg2 and imposm.parser. It also requires access to a postgresql database with OSM data for the area imported with osmosis using a pgsnapshot schema. This database will normally be on the same machine as addressmerge although it does not have to be.
 
 To install the minimal requirements for addressmerge and imposm on Ubuntu, using virtualenv
 
 ```
 sudo apt-get install build-essential python-devel protobuf-compiler \
-					 libprotobuf-dev python-psycopg2 python-pip \
+					 libprotobuf-dev python-psycopg2 python-pip
 ```
 
 You can then install imposm for everyone or in a virtual environment
@@ -27,12 +27,14 @@ If running with a virtual environment you need to do ```source venv/bin/activate
 
 It is possible to install ```imposm.parser``` with [less dependencies](http://dev.omniscale.net/imposm.parser/index.html#document-install) than ```imposm```.
 
-Installation of PostgreSQL+PostGIS is beyond the scope of this readme.
+Installation of osmosis and PostgreSQL+PostGIS is beyond the scope of this readme.
 
 ## Importing data ##
-To import the data with imposm from ```dump.osm.pbf``` into the database ```osm``` use the command
+It is possible to use an existing pgsnapshot database kept up to date with minutely diffs. This is beyond the scope of this readme.
+
+To import the data with imposm from ```dump.osm.pbf``` into the database ```osm``` with postgis and hstore already set up on the database use the commands
 
 ```
-imposm -d osm -m addressmapping.py --overwrite-cache --read dump.osm.pbf --write --deploy-production-tables --remove-backup-tables
-psql -d osm -f indexes.sql
+psql -d osm -f <path-to-osmosis>/script/pgsnapshot_schema_0.6.sql
+osmosis --read-pbf dump.osm.pbf --write-pgsql host=localhost database=osm user=osm
 ```
--- addressmerge.py
+++ addressmerge.py
@@ -15,24 +15,78 @@ from imposm.parser import OSMParser
 from lxml import etree
 
 class OSMSource(object):
-    def __init__(self, database, user, password, host, port):
+    def __init__(self, database, user, password, host, port, ewkt):
         l.debug('Connecting to postgresql')
         self._conn=psycopg2.connect(database=database, user=user, 
                                     password=password, host=host, 
                                     port=str(port))
         self._conn.set_session(readonly=False, autocommit=False)
-        self._table_created = False
+        self.ewkt = ewkt
+        self.validate_ewkt()
+        self.create_table()
+
+    def validate_ewkt(self):
+        '''
+        This function checks that self.ewkt is a valid EWKT string. It will also fail if
+        the DB does not have PostGIS enabled or it could not connect.
+        '''
+        curs = None
+        try:
+            curs = self._conn.cursor()
+            curs.execute('''SELECT %s::geometry;''', (self.ewkt,))
+            curs.connection.rollback()
+        except BaseException:
+            if curs is not None:
+                curs.connection.rollback()
+            raise
+        finally:
+            if curs is not None:
+                curs.close()
 
     def create_table(self):
         curs = None
         try:
             curs = self._conn.cursor()
-            curs.execute('''CREATE TEMPORARY TABLE import_addresses (
-                            import_id integer,
+            curs.execute('''CREATE TEMPORARY TABLE import_addresses
+                            (import_id integer,
                             "addr:housenumber" varchar(255),
                             "addr:street" varchar(255),
                             "addr:city" varchar(255));''')
-            self._table_created = True
+
+            curs.execute('''CREATE TEMPORARY VIEW local_nodes AS
+                            SELECT id, tags, geom FROM nodes
+                            WHERE ST_Intersects(geom,%s::geometry);''',
+                            (self.ewkt,))
+
+            curs.execute('''CREATE TEMPORARY VIEW local_ways AS
+                            SELECT id, tags, linestring AS geom FROM ways
+                            WHERE ST_Intersects(linestring,%s::geometry);''',
+                            (self.ewkt,))
+
+            curs.execute('''CREATE TEMPORARY VIEW local_mps AS
+                            SELECT relation_id AS id,
+                                relation_tags AS tags,
+                                ST_MakeLine(relation_ways.linestring) AS geom
+                            FROM (SELECT relations.id AS relation_id,
+                                relations.tags AS relation_tags, geom AS linestring
+                            FROM local_ways JOIN relation_members
+                            ON (local_ways.id = relation_members.member_id
+                            AND relation_members.member_type='W')
+                            JOIN relations
+                            ON (relation_members.relation_id = relations.id)
+                            WHERE relations.tags @> hstore('type','multipolygon'))
+                            AS relation_ways
+                            GROUP BY relation_id, relation_tags;''')
+
+            curs.execute('''CREATE TEMPORARY TABLE local_all
+                            AS SELECT local_nodes.id, 'N'::character(1) AS type,
+                                local_nodes.tags, local_nodes.geom FROM local_nodes
+                            UNION ALL SELECT local_ways.id, 'W'::character(1) AS type,
+                                local_ways.tags, local_ways.geom FROM local_ways
+                            UNION ALL SELECT local_mps.id, 'M'::character(1) AS type,
+                                local_mps.tags, local_mps.geom FROM local_mps;''')
+
+            curs.execute('''ANALYZE local_all;''');
             curs.connection.commit()
         except BaseException:
             if curs is not None:
@@ -43,7 +97,6 @@ class OSMSource(object):
                 curs.close()
 
     def load_addresses(self, addresses):
-        self.create_table()
         curs = None
         try:
             curs = self._conn.cursor()
@@ -66,7 +119,11 @@ class OSMSource(object):
         curs = None
         try:
             curs = self._conn.cursor()
-            curs.execute('''SELECT DISTINCT ON (import_id) import_id FROM osm_addresses JOIN import_addresses USING ("addr:housenumber", "addr:street", "addr:city");''')
+            curs.execute('''SELECT DISTINCT ON (import_id) import_id FROM local_all
+            JOIN import_addresses
+            ON tags -> 'addr:housenumber' = "addr:housenumber"
+            AND tags -> 'addr:street' = "addr:street"
+            AND tags -> 'addr:city' = "addr:city";''')
             return set(id[0] for id in curs.fetchall())
         except BaseException:
             if curs is not None:
@@ -100,7 +157,6 @@ class ImportDocument(object):
         f.write('\n')
 
     def remove_existing(self, source):
-        ''' This function will remove addresses that already exist in OSM'''
         l.debug('Loading addresses')
         source.load_addresses(self._nodes)
         l.debug('Finding duplicates')
@@ -136,11 +192,16 @@ if __name__ == '__main__':
     parser.add_argument('input', help='Input OSM file')
     parser.add_argument('output', type=argparse.FileType('w'), help='Output OSM file')
 
+    # processing options
+
+    parser.add_argument('-e', '--ewkt', type=argparse.FileType('r'), help='Well-known text (EWKT) file with a POLYGON or other area type to search for addresses in')
+
     args = parser.parse_args()
 
     existing = OSMSource( database=args.dbname, user=args.username,
                           password=args.password, host=args.host,
-                          port=str(args.port))
+                          port=str(args.port),
+                          ewkt=args.ewkt.read())
 
     source = ImportDocument()
 
